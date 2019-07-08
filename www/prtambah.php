0d12c5459ad977fe58c1ddec5dad70a136451703
rhesanic <rhesanicop@gmail.com>
2019-07-09 00:08:01 +0700

    q

--- www/prtambah.php
+++ www/prtambah.php
@@ -6,16 +6,16 @@
 		$email = $_POST['email'];
 		$alamat = $_POST['alamat'];
 	
-	$query = 'insert into anggota (nama,email,alamat) values ('.$nama.','.$email.','.$alamat.')';
+	$query = 'insert into anggota (nama,email,alamat) values ("$nama","$email","$alamat")';
     $result = mysqli_query($conn, $query);	
 	if(!$result){
-								echo "Gagal Tambah Mobil!</br>";
+								echo "Gagal Tambah data!</br>";
 								echo mysqli_error($conn);
-								echo "<form action='tambah.html'>
+								echo "<form action='tambah.php'>
 										<input type='submit' onClick='self.history.back()' value='Kembali' />
 										</form>";
 										}else {
-								echo "Berhasil Tambah Mobil!</br>";
+								echo "Berhasil Tambah data!</br>";
 								echo "silahkan <a href='index.php'>index</a>";	
 								}
 	
